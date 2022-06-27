dc0fed9661d5459d3ddaa2d60283ecabdb0a1d4f
anandu <anandu.babu@bourntec.com>
2022-06-27 15:10:46 +0530

    first commit

--- src/main/java/com/example/letsLearn/LetsLearnApplication.java
+++ src/main/java/com/example/letsLearn/LetsLearnApplication.java
@@ -9,25 +9,25 @@ import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
 
 @SpringBootApplication
 public class LetsLearnApplication {
 
 	public static void main(String[] args) throws IOException {
 
-		ClassLoader classLoader =  LetsLearnApplication.class.getClassLoader();
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-		System.out.println(file);
-		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
-		
-		FirebaseOptions options = new FirebaseOptions.Builder()
-		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-		  .build();
-
-		FirebaseApp.initializeApp(options);
+//		ClassLoader classLoader =  LetsLearnApplication.class.getClassLoader();
+//		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+//		System.out.println(file);
+//		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+//		
+//		FirebaseOptions options = new FirebaseOptions.Builder()
+//		  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//		  .build();
+//
+//		FirebaseApp.initializeApp(options);
 		
 		SpringApplication.run(LetsLearnApplication.class, args);
 	}
--- src/main/java/com/example/letsLearn/service/UserService.java
+++ src/main/java/com/example/letsLearn/service/UserService.java
@@ -5,25 +5,25 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.stereotype.Service;
 
 import com.example.letsLearn.entity.User;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.firebase.cloud.FirestoreClient;
+//import com.google.api.core.ApiFuture;
+//import com.google.cloud.firestore.DocumentReference;
+//import com.google.cloud.firestore.DocumentSnapshot;
+//import com.google.cloud.firestore.Firestore;
+//import com.google.firebase.cloud.FirestoreClient;
 
 @Service
 public class UserService {
 
 	public User getUsers(String documentId) throws InterruptedException, ExecutionException {
-		Firestore dbFireStore = FirestoreClient.getFirestore();
-		DocumentReference documentReference = dbFireStore.collection("users").document(documentId);
-		ApiFuture<DocumentSnapshot> future = documentReference.get();
-		DocumentSnapshot document = future.get();
-		User user;
-		if(document.exists()) {
-			user=document.toObject(User.class);
-			return user;
-		}
+//		Firestore dbFireStore = FirestoreClient.getFirestore();
+//		DocumentReference documentReference = dbFireStore.collection("users").document(documentId);
+//		ApiFuture<DocumentSnapshot> future = documentReference.get();
+//		DocumentSnapshot document = future.get();
+//		User user;
+//		if(document.exists()) {
+//			user=document.toObject(User.class);
+//			return user;
+//		}
 		return null;
 	}
 
